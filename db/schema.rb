5eb3b384ec4b92c65876b3fdacb5ea15a326e2cf
decareano <decareano@users.noreply.github.com>
2014-09-24 22:12:24 -0700

    added captchaExample.js

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140913183322) do
+ActiveRecord::Schema.define(version: 20140914202645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -143,6 +143,13 @@ ActiveRecord::Schema.define(version: 20140913183322) do
 
   add_index "projects", ["slug"], name: "index_projects_on_slug", unique: true, using: :btree
   add_index "projects", ["user_id"], name: "index_projects_on_user_id", using: :btree
+
+  create_table "scrums", force: true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "youtube_url"
+  end
 
   create_table "static_pages", force: true do |t|
     t.string   "title"
